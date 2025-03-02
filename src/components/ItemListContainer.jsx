ca77ac0f2c8b3ab1bc56795c7426220cd4bf2b07
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-02 20:08:26 -0300

    se modifica el archivo ItemListContainer.jsx

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,12 +1,1 @@
-//src/components/ItemListContainer.jsx
-import React from "react";
-
-const ItemListContainer = ({ greeting }) => {
-    return (
-        <div className="item-list-container">
-            <h2>{greeting}</h2>
-        </div>
-    );
-};
-
-export default ItemListContainer;
+export const ItemListContainer = () =>
