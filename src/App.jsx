834d95497d934df66db237e678bec1a27ce8f62a
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-02 19:52:31 -0300

    se modifica App.jsx para que se vea el NavBar correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar } from './components/NavBar';
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
   return (
   <>  
   <Navbar />
+  <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:categoryId" element={<Category />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="*" element={<h2>404 Not Found</h2>} />
+      </Routes>
   </>
-  )
+  );
 }
 
 export default App
