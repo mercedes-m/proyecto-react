a425eb56b95d79a5aa311bd0d1e96edd73f60a04
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-02 20:51:36 -0300

    se crea la carpeta data y se agrega contenido en el archivo data.js

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+const items = [
+    {
+      id: 1,
+      imagen: "https://source.unsplash.com/400x300/?nature",
+      titulo: "Paisaje Natural",
+      descripcion: "Una hermosa vista de la naturaleza con montañas y un lago cristalino."
+    },
+    {
+      id: 2,
+      imagen: "https://source.unsplash.com/400x300/?technology",
+      titulo: "Innovación Tecnológica",
+      descripcion: "Un primer plano de circuitos electrónicos que representan la evolución digital."
+    },
+    {
+      id: 3,
+      imagen: "https://source.unsplash.com/400x300/?architecture",
+      titulo: "Arquitectura Moderna",
+      descripcion: "Un impresionante rascacielos con un diseño vanguardista y líneas minimalistas."
+    }
+  ];
+  
+  console.log(items);
