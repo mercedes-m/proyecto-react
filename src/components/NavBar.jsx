2ff411728eee4a62c6c8d2eab5d8eec220c31f76
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-02 19:40:37 -0300

    se modifica NavBar.jsx para que muestre las categorías

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,15 +5,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
-
 export const NavBar = () => (
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/">Features</Nav.Link>
+            <Nav.Link as={Link} to="/">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
