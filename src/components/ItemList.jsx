ba2dbfc8986ef06e2dda64847f6dfacb5648dfdc
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 13:46:55 -0300

    se elimina la card de ItemList.jsx

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,15 +2,5 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 export const ItemList = ({ items }) =>
-    {items.map((item) => (
-        <Card key={item.id} style={{ width: '18rem', margin: '10px' }}>
-        <Card.Img variant="top" src={item.imagen} alt={item.titulo} />
-        <Card.Body>
-          <Card.Title>{item.titulo}</Card.Title>
-          <Card.Text>
-           {item.descripcion}
-          </Card.Text>
-          <Button variant="primary">Agregar al Carrito</Button>
-        </Card.Body>
-      </Card>
-      ))}   
+ items.map((item) => <Item item={items} />);
+        
