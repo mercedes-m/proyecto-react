40cbffa04a45726f8f9a2eb2121a7c765678a865
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 13:11:04 -0300

    se agrega una card en ItemList.jsx

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,13 @@
+export const ItemList = () =>
+    {items.map((item) => (
+        <Card key={item.id} style={{ width: '18rem', margin: '10px' }}>
+        <Card.Img variant="top" src={item.imagen} alt={item.titulo} />
+        <Card.Body>
+          <Card.Title>{item.titulo}</Card.Title>
+          <Card.Text>
+           {item.descripcion}
+          </Card.Text>
+          <Button variant="primary">Agregar al Carrito</Button>
+        </Card.Body>
+      </Card>
+      ))}   
