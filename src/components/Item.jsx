87cbe0dbc07ec2d3d5f84e8fd0819cf6eb96bb4c
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 15:58:31 -0300

    se modifica la card y se agrega reac-router-dom en Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,22 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
 
-    export const ItemList = ({ items }) =>
-        {items.map((item) => (
+    export const ItemList = ({ items }) => {
+        return (
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>   
+          {items.map((item) => (
             <Card key={item.id} style={{ width: '18rem', margin: '10px' }}>
             <Card.Img variant="top" src={item.imagen} alt={item.titulo} />
             <Card.Body>
               <Card.Title>{item.titulo}</Card.Title>
-              <Card.Text>
-               {item.descripcion}
-              </Card.Text>
+              <Card.Text>{item.descripcion}</Card.Text>
+              <Link to={`/item/${item.id}`}></Link>
               <Button variant="primary">Agregar al Carrito</Button>
-            </Card.Body>
-          </Card>
-          ))}   
+            </Link>
+        </Card.Body>
+     </Card>
+   ))}   
+ </div>
+ );
+};
