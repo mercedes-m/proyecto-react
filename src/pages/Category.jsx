95302e85d7e3d43935b85a72be16ab8e2062dd4a
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 16:31:13 -0300

    se agrega el contenido al archivo Category.jsx

---
+++ src/pages/Category.jsx
@@ -0,0 +1,13 @@
+// src/pages/Category.jsx
+import React from 'react';
+
+const Category = () => {
+  return (
+    <div>
+      <h2>Categoría</h2>
+      {/* agregar la lógica para mostrar productos filtrados por categoría */}
+    </div>
+  );
+};
+
+export default Category;
