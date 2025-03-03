1c2e5beeff3cbf780c30e435ba354c02cfd14a01
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 16:59:45 -0300

    se corrigen errores en ItemList.jsx

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,22 +1,17 @@
+ // src/components/ItemList.jsx
 import React from 'react';
 import Item from './Item';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
-export const ItemList = ({ items }) => {
+const ItemList = ({ items }) => {
   return (
-    <div className="item-list">
-      {items.map((item) => (
-        <Card key={item.id} style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={item.imagen} />
-          <Card.Body>
-            <Card.Title>{item.titulo}</Card.Title>
-            <Card.Text>{item.descripcion}</Card.Text>
-            <Button as={Link} to={`/product/${item.id}`} variant="primary">Ver Detalle</Button>
-          </Card.Body>
-        </Card>
+    <div className="row">
+      {items.map(item => (
+        <div className="col-md-4" key={item.id}>
+          <Item item={item} />
+        </div>
       ))}
     </div>
   );
 };
-        
+
+export default ItemList;
