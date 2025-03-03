8dac002204b64a61f880eaeb4f9476b038f73b9a
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 12:30:04 -0300

    se modifica ItemListContainer.jsx para que los items

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,19 +1,18 @@
 // src/components/ItemListContainer.jsx
-import React from 'react';
 import Container from 'react-bootstrap/Container';
 import { items } from '../data/data';
 
-const ItemListContainer = ({ products }) => {
-  return (
-    <div>
+const ItemListContainer = () => (
+    <Container>
       <h2>Listado de Productos</h2>
-      <ul>
-        {products.map(product => (
-          <li key={product.id}>{product.name}</li>
-        ))}
-      </ul>
+      {items.map((item) => (
+      <div key={item.id}>
+      <img src={item.imagen} alt={item.titulo} style={{ width: '100%', height: 'auto' }} />
+      <h3>{item.titulo}</h3>
+      <p>{item.descripcion}</p>
     </div>
+      ))}
+    </Container>
   );
-};
 
-export default ItemListContainer;
+  export default ItemListContainer;
