2b5ddb73a6da20f93d7372f47cd5ef3e035de496
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 13:18:47 -0300

    se elimina la card en ItemListContainer.jsx

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,24 +3,11 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { items } from '../data/data';
+import { itemsList } from './ItemList';
 
 const ItemListContainer = () => (
     <Container classname= "d-flex mt-4">
-      <h2>Listado de Productos</h2>
-      <div className="d-flex flex-wrap justify-content-around">
-      {items.map((item) => (
-        <Card key={item.id} style={{ width: '18rem', margin: '10px' }}>
-        <Card.Img variant="top" src={item.imagen} alt={item.titulo} />
-        <Card.Body>
-          <Card.Title>{item.titulo}</Card.Title>
-          <Card.Text>
-           {item.descripcion}
-          </Card.Text>
-          <Button variant="primary">Agregar al Carrito</Button>
-        </Card.Body>
-      </Card>
-      ))}
-    </div>
+     <ItemList items={ items } />
     </Container>
     );
 
