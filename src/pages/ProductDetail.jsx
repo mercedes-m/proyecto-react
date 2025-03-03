fd0fe0ab2c1d5c7ad678594beb8c27bf2fe9da2e
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 16:32:46 -0300

    se agrega el contenido al archivo ProductDetail.jsx

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,13 @@
+// src/pages/ProductDetail.jsx
+import React from 'react';
+
+const ProductDetail = () => {
+  return (
+    <div>
+      <h2>Detalle del Producto</h2>
+      {/* agregar la lógica para mostrar los detalles del producto */}
+    </div>
+  );
+};
+
+export default ProductDetail;
