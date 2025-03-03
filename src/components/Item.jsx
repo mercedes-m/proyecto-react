c2614982716ab0b051f27d65790f94917adda3a6
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 16:01:26 -0300

    se corrigen errores en en el archivo item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,21 +2,21 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
 
-    export const ItemList = ({ items }) => {
-        return (
-        <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>   
-          {items.map((item) => (
-            <Card key={item.id} style={{ width: '18rem', margin: '10px' }}>
+export const ItemList = ({ items }) => {
+    return (
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+        {items.map((item) => (
+          <Card key={item.id} style={{ width: "18rem", margin: "10px" }}>
             <Card.Img variant="top" src={item.imagen} alt={item.titulo} />
             <Card.Body>
               <Card.Title>{item.titulo}</Card.Title>
               <Card.Text>{item.descripcion}</Card.Text>
-              <Link to={`/item/${item.id}`}></Link>
-              <Button variant="primary">Agregar al Carrito</Button>
-            </Link>
-        </Card.Body>
-     </Card>
-   ))}   
- </div>
- );
-};
+              <Link to={`/item/${item.id}`}>
+                <Button variant="primary">Ver Detalle</Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    );
+  };  
