d4e7fbed8a25e4e07f4ed37f4a16073a2c3a6a9a
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 17:04:06 -0300

    se modifica NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,19 @@
-//src/componente/NavBar.jsx
+// src/components/NavBar.jsx
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import CartWidget from './CartWidget';
 
-export const NavBar = () => (
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/">Features</Nav.Link>
-            <Nav.Link as={Link} to="/">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-);
+const NavBar = ({ itemCount }) => {
+  return (
+    <Navbar bg="primary" variant="dark">
+      <Navbar.Brand as={Link} to="/">Mi Tienda</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+        <Nav.Link as={Link} to="/category/electronics">Electrónica</Nav.Link>
+        <Nav.Link as={Link} to="/category/clothing">Ropa</Nav.Link>
+      </Nav>
+      <CartWidget itemCount={itemCount} />
+    </Navbar>
+  );
+};
