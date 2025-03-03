fb7ff4c8d077d9d635ddcad7c5d8460ed24623af
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 16:12:45 -0300

    se aplican cambios en ItemList.jsx para mostrar los productos y permitir la navegación

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,6 +1,22 @@
-import Button from 'react-bootstrap/Button';
+import React from 'react';
+import Item from './Item';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-export const ItemList = ({ items }) =>
- items.map((item) => <Item item={item} />);
+export const ItemList = ({ items }) => {
+  return (
+    <div className="item-list">
+      {items.map((item) => (
+        <Card key={item.id} style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={item.imagen} />
+          <Card.Body>
+            <Card.Title>{item.titulo}</Card.Title>
+            <Card.Text>{item.descripcion}</Card.Text>
+            <Button as={Link} to={`/product/${item.id}`} variant="primary">Ver Detalle</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
+};
         
