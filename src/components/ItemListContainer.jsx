9dd266a4791b2018c8650ac48dffb93fb54b968f
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 13:49:38 -0300

    se modifica el archivo ItemListContainer.jsx

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,6 @@
 // src/components/ItemListContainer.jsx
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+
 import { items } from '../data/data';
 import { itemsList } from './ItemList';
 
