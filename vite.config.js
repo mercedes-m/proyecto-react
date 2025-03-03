828eafe9d5b6de117fa70d51d626bf9164b29b26
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 17:31:48 -0300

    se modifica la ruta del proyecto en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mercedes-m.github.io/proyecto-react/",
+  base: "/proyecto-react/",
 });
