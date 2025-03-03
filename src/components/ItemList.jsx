3ad699c850e56fbe27fa889d134ccdc6872282d0
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-03 13:22:58 -0300

    se agregan items, card y button de bootstrap

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,4 +1,7 @@
-export const ItemList = () =>
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export const ItemList = ({ items }) =>
     {items.map((item) => (
         <Card key={item.id} style={{ width: '18rem', margin: '10px' }}>
         <Card.Img variant="top" src={item.imagen} alt={item.titulo} />
