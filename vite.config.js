d1af0331b16285a858edae9073ee45c62f88d8b2
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-04 11:19:18 -0300

    se corrige un error en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/proyecto-react/",
   server: {
     port: 5173,
-  base: "/proyecto-react/",
+},
 });
