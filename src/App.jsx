b4beb4e4d8d9d63a3291fa828d5b3ad4c35e52de
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-04 12:13:45 -0300

    se corrige la importanción de ItemListContainer en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  NavBar  from './components/NavBar';
-import { Routes, Route } from 'react-router-dom';
+import  { Routes, Route } from 'react-router-dom';
 import './App.css'
-import { ItemListContainer } from './components/ItemListContainer';
+import  ItemListContainer from './components/ItemListContainer';
 import Home from './pages/Home'; 
 import Category from './pages/Category'; 
 import ProductDetail from './pages/ProductDetail';
