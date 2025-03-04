42f83d6b9fdd353f52ec24c48208b9dd62703823
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-04 14:06:33 -0300

    se modifican los links de las imágenes en data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,22 +1,26 @@
+import buzoBlanco from '../images/buzo blanco.jpg';
+import sweaterRayado from '../images/sweater rayado.jpg'; 
+import sweaterBlanco from '../images/sweater blanco.jpg';
+
 export const items = [
     {
       id: 1,
-      imagen: require('../images/buzo blanco.jpg'),
+      imagen: buzoBlanco,
       titulo: "Buzo Blanco",
       descripcion: "Buzo blanco de algodón."
     },
     {
       id: 2,
-      imagen: require('../images/swater rayado.jpg'),
+      imagen: sweaterRayado,
       titulo: "Sweater Rayado",
       descripcion: "Sweater rayado de hilo azul y blanco."
     },
     {
       id: 3,
-      imagen: require('../images/sweater blanco.jpg'),
+      imagen: sweaterBlanco,
       titulo: "Sweater Blanco",
-      descripcion: "Sweater blanco de lana con cuello alto."
-    }
+      descripcion: "Sweater blanco de lana con cuello alto.",
+    },
   ];
   
   console.log(items);
