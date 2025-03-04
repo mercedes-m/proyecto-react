edd7adec3122539887d0442bb03d529f9124f2a2
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-04 12:01:22 -0300

    se corrige un error de tipeo en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from './components/NavBar';
+import  NavBar  from './components/NavBar';
 import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import { ItemListContainer } from './components/ItemListContainer';
@@ -10,16 +10,15 @@ import ProductDetail from './pages/ProductDetail';
 function App() {
   return (
   <>  
-  <Navbar />
+  <NavBar />
   <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/category/:categoryId" element={<ItemListContainer />} />
         <Route path="/product/:productId" element={<ProductDetail />} />
         <Route path="*" element={<h2>404 Not Found</h2>} />
       </Routes>
-      <ItemListContainer/>
   </>
   );
 }
 
-export default App
+export default App  
