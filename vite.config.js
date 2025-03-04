bb898fc6c18b33cd0defa6a93beb0fb7efe4aff3
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-04 14:28:31 -0300

    se corrige la base en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mercedes-m.github.io/proyecto-react/",
+  base: "/proyecto-react/",
   server: {
     port: 5173,
 },
