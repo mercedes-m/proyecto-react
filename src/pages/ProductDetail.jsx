ab3134039208b9fe06e8d969390f65fd84de1b7b
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-10 12:53:34 -0300

    se modifica el archivo para mostrar los detalles de productos

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,10 +1,14 @@
 // src/pages/ProductDetail.jsx
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
+  const { useParams } = useParams(); //Obtiene el productId de la URL
+
   return (
     <div>
       <h2>Detalle del Producto</h2>
+      <p>ID del producto: {productId}</p>
       {/* agregar la lógica para mostrar los detalles del producto */}
     </div>
   );
