488c60ac56d02eeb022f838b0e8d017b2f7a2945
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-10 13:00:03 -0300

    se corrige un error en el código

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App.jsx';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom'; // Importa BrowserRouter
 
 // Crea el root y renderiza la aplicación
