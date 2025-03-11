6e89294ad95c055080abae1a181b82b37bcc2b2c
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-03-11 07:58:14 -0300

    se corrigen errores de sintaxis en Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Item = ({ items }) => {
+const Item = ({ item }) => {
   return (
-    <div className="card" style={{ width: '18rem' }}>
+    <div className="card" style={{ width: '18rem', margin: '1rem' }}>
       <img src={item.imagen} className="card-img-top" alt={item.titulo} />
       <div className="card-body">
         <h5 className="card-title">{item.titulo}</h5>
